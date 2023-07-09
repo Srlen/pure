4f214c432053470ab619a107b6765ab7778d2e32
Mohmmad Alattar <xsrlen@gmail.com>
2023-07-10 00:13:37 +0300

    update test

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { AppModule } from './app.module';
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
+      imports: [AppModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
